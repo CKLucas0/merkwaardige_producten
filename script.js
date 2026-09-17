d7dd1e22495e53a0a84530c0bc2eacef62f268c8
CKLucas0 <lucasvandeweijerschool@gmail.com>
2026-09-17 23:19:47 +0200

    made the whole website

---
+++ script.js
@@ -0,0 +1,24 @@
+// vars
+// list = document.getElementById("list")
+exponents = ["","²","³","⁴","⁵","⁶","⁷","⁸"]
+
+// functions
+function randint(min, max){
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+function oefening() {
+    a = randint(2,12)
+    b = randint(2,12)
+    ex1 = randint(0,3)
+    ex2 = randint(0,3)
+
+    return ("(" + a + "a" + exponents[ex1] + " + " + b + "b" + exponents[ex2] + ")² = " + a**2 + "a" + exponents[(ex1*2)+1] + " + " + 2*a*b + "a" + exponents[ex1]+"b" + exponents[ex2] + " + " + b**2 + "b" + exponents[(ex2*2)+1] )
+}
+
+function AddToList() {
+    const li = document.createElement("li")
+    li.textContent = oefening()
+    list.append(li)
+}
+
